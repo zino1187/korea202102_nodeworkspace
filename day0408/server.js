e5075074ba84c57a289227c300827e58e1141c7c
zino1187 <zino1187@gmail.com>
2021-04-08 16:57:59 +0900

    c

--- day0408/server.js
+++ day0408/server.js
@@ -4,7 +4,8 @@ var multer=require("multer"); //외부
 //var oracledb = require("oracledb");//외부
 var mysql=require("mysql");
 var mymodule = require("./lib/mymodule.js");
-
+var fs=require("fs");
+var ejs=require("ejs");
 var express = require("express");
 
 var app = express();//express객체 생성 
@@ -34,6 +35,33 @@ var conStr={
     database:"nodejs"
 };
 
+//글목록 요청 처리 
+app.get("/gallery/list", function(request, response){
+    var con = mysql.createConnection(conStr); //접속 
+
+    var sql="select * from gallery order by gallery_id desc"; //내림차순 
+    con.query(sql, function(err, result, fields){
+        if(err){
+            console.log(err);
+        }else{
+            //ejs렌더링..
+            fs.readFile("./gallery/list.ejs", "utf8", function(error, data){
+                if(error){
+                    console.log(error);
+                }else{
+                    response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+                    response.end(ejs.render(data, {
+                        galleryList:result, 
+                        lib:mymodule
+                    }));//ejs렌더링해서 넣기
+                }
+            });
+        }
+        con.end();
+    }); 
+
+});
+
 //글등록 요청 처리 
 app.post("/gallery/regist", upload.single("pic") , function(request , response){
     //파라미터 받기 
@@ -56,6 +84,35 @@ app.post("/gallery/regist", upload.single("pic") , function(request , response){
         con.end(); //mysql 접속 해제
     });
 });
+
+//상세보기 요청 
+app.get("/gallery/detail", function(request, response){
+    var con=mysql.createConnection(conStr);
+    var gallery_id=request.query.gallery_id;
+    var sql="select * from gallery where gallery_id="+gallery_id;
+
+    //쿼리문 수행
+    con.query(sql, function(err, result, fields){   
+        if(err){
+            console.log(err);
+        }else{
+            //상세페이지 보여주기 
+            fs.readFile("./gallery/detail.ejs", "utf8", function(error, data){
+                if(error){
+                    console.log(error);
+                }else{
+                    var d=ejs.render(data, {
+                        gallery:result[0]  // result가 한건의 데이터만 담고 있다고 하더라도, 배열이므로...
+                    });  
+                    response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+                    response.end(d);
+                }
+                con.end(); //mysql close
+            });               
+        }        
+    });
+});
+
 
 var server = http.createServer(app); //기본 모듈에 express 모듈 연결 
 server.listen(9999, function(){
