7abd2114df959e9e27245df59cf5c8501ff77e33
zino1187 <zino1187@gmail.com>
2021-04-08 10:50:48 +0900

    c

--- day0408/server.js
+++ day0408/server.js
@@ -6,8 +6,6 @@ var app = express();//express객체 생성
 //필요한 각종 미들웨어 적용 
 app.use(express.static(__dirname+"/static"));
 
-//3분에
-
 var server = http.createServer(app); //기본 모듈에 express 모듈 연결 
 server.listen(9999, function(){
     console.log("Gallery Server is running at 9999 port...");
