e1a3cf93499be511937d93bc4b644cff970cea17
zino1187 <zino1187@gmail.com>
2021-04-09 14:26:11 +0900

    c

--- day0409/server.js
+++ day0409/server.js
@@ -1,12 +1,22 @@
 var http = require("http");
-var express = require("express");
+var express = require("express"); //외부
 var fs=require("fs");
-var ejs =require("ejs"); 
+var ejs =require("ejs"); //외부
 
 var app = express();
 
 //게시판 목록 요청 처리 
 app.get("/comments/list", function(request, response){
+
+    fs.readFile("./comments/list.ejs", "utf8", function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            var r = ejs.render(data,{}); //ejs 해석 및 실행하기!!
+            response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+            response.end(r); //실행한 결과 전송하기!!
+        }
+    });
 
 });
 
