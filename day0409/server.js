8c01ed6887810e1864aada566dcb2cbe9d2f7421
zino1187 <zino1187@gmail.com>
2021-04-12 11:47:59 +0900

    c

--- day0409/server.js
+++ day0409/server.js
@@ -3,7 +3,12 @@ var express = require("express"); //외부-설치
 var fs=require("fs");
 var ejs =require("ejs"); //외부-설치
 var oracledb=require("oracledb");//외부-설치
+var static = require("serve-static"); //정적자원을 처리하기위한 모듈
+
 var app = express();
+
+//미들웨어 등록 
+app.use(static(__dirname+"/static")); //정적자원의 루트 디렉토리 등록!!!
 
 //이 시점 이후 부터는 conStr변수의 값은 변할 수 없다~~(상수화 시킴)
 const conStr={
@@ -28,24 +33,39 @@ app.get("/comments/list", function(request, response){
             console.log("접속실패",err);
         }else{
             console.log("접속성공");
+            //쿼리문 실행 
+            var sql="select * from news order by news_id desc";
+
+            con.execute(sql, function(error, result){
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log("result는 ",result);
+
+                    fs.readFile("./comments/list.ejs", "utf8", function(error, data){
+                        if(error){
+                            console.log(error);
+                        }else{
+                            var r = ejs.render(data,{
+                                //ejs에 넘겨줄 데이터 지정 
+                                param:{
+                                    page:currentPage,
+                                    /*result 는 mysql과 틀리게 json객체의 rows 속성에 
+                                    2차원배열에 들어있다..*/
+                                    record:result.rows,
+                                }
+                            }); //ejs 해석 및 실행하기!!
+                            response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+                            response.end(r); //실행한 결과 전송하기!!
+                        }
+                    });
+                }
+                con.close();
+            });
         }
     }); //오라클 접속 및 접속객체 가져오기
-
-    fs.readFile("./comments/list.ejs", "utf8", function(error, data){
-        if(error){
-            console.log(error);
-        }else{
-            var r = ejs.render(data,{
-                //ejs에 넘겨줄 데이터 지정 
-                param:{
-                    page:currentPage
-                }
-            }); //ejs 해석 및 실행하기!!
-            response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
-            response.end(r); //실행한 결과 전송하기!!
-        }
-    });
-
+    
+    
 });
 
 var server = http.createServer(app);
